6cbc57fccf917951d7eb7a248a4afd00302bf29a
Nils Uhrberg <nils.uhrberg@codecentric.de>
2025-04-05 18:22:21 +0200

    refactor: Add solution

--- src/titanicsurvivors/pipelines/deploy/deploy_xgboost.py
+++ src/titanicsurvivors/pipelines/deploy/deploy_xgboost.py
@@ -5,6 +5,7 @@ from zenml import pipeline
 from zenml.client import Client
 from zenml.integrations.bentoml.steps import (
     bento_builder_step,
+    bentoml_model_deployer_step,
 )
 
 from titanicsurvivors.models import titanic_xgboost
@@ -16,12 +17,12 @@ load_dotenv()
 @pipeline(model=titanic_xgboost, enable_cache=False)
 def bento_deployment_pipeline():
     model_artifact = Client().get_artifact_version(
-        f"<model_artifact>{os.getenv('GROUP_NAME', 'Default')}"
+        f"xgb_model_{os.getenv('GROUP_NAME', 'Default')}"
     )  # ... Change the name of the model artifact. Exchange with a Datat Scientist what name was given to the model artifact.
 
     bento = bento_builder_step(
         model=model_artifact,
-        model_name="<model_name>",  # ... Add a model name. This must match the one in the service.
+        model_name="titanic-classifier",  # ... Add a model name. This must match the one in the service.
         model_type="xgboost",
         service="service.py:TitanicService",
         labels={
@@ -32,6 +33,22 @@ def bento_deployment_pipeline():
         python={
             "packages": ["zenml", "xgboost", "pandas", "scikit-learn"],
         },
+    )
+
+    # bentoml_model_deployer_step(
+    #     bento=bento,
+    #     model_name="titanic-classifier",  # Name of the model
+    #     port=3001,  # Port to be used by the http server
+    #     deployment_type="container",
+    #     image="mlopsworkshop/titanicsurvivors",
+    #     image_tag=titanicsurvivors.__version__,
+    #     platform="linux/amd64",
+    # )
+
+    deployed_model = bentoml_model_deployer_step(
+        bento=bento,
+        model_name="titanic-classifier",  # Name of the model
+        port=3001,  # Port to be used by the http server
     )
 
 
